77e3cef3a111771781a33e8dab78b2eceff0b581
sujan <sujan64rai@gmail.com>
2025-12-28 23:13:56 +0000

    feat: add @netlify/vite-plugin dependency to package.json

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react';
 import tanstackRouter from '@tanstack/router-plugin/vite';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
+import netlify from "@netlify/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    netlify(),
     tailwindcss(),
     tanstackRouter({
       target: 'react',
